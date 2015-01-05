17b7c341988e0c850274e87ec386e77396b781ce
katyeneza <yenezak@pagosa.k12.co.us>
2015-01-05 10:26:26 -0700

    k

---
+++ StringIntro/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtStrInput = New System.Windows.Forms.TextBox()
+        Me.btnChars = New System.Windows.Forms.Button()
+        Me.btnLength = New System.Windows.Forms.Button()
+        Me.btnToUpper = New System.Windows.Forms.Button()
+        Me.btnToLower = New System.Windows.Forms.Button()
+        Me.btnTrim = New System.Windows.Forms.Button()
+        Me.btnSubstring = New System.Windows.Forms.Button()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.btnReplace = New System.Windows.Forms.Button()
+        Me.btnInsert = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtStrInput
+        '
+        Me.txtStrInput.Location = New System.Drawing.Point(110, 26)
+        Me.txtStrInput.Name = "txtStrInput"
+        Me.txtStrInput.Size = New System.Drawing.Size(53, 20)
+        Me.txtStrInput.TabIndex = 0
+        '
+        'btnChars
+        '
+        Me.btnChars.Location = New System.Drawing.Point(22, 76)
+        Me.btnChars.Name = "btnChars"
+        Me.btnChars.Size = New System.Drawing.Size(76, 23)
+        Me.btnChars.TabIndex = 1
+        Me.btnChars.Text = "Chars"
+        Me.btnChars.UseVisualStyleBackColor = True
+        '
+        'btnLength
+        '
+        Me.btnLength.Location = New System.Drawing.Point(21, 117)
+        Me.btnLength.Name = "btnLength"
+        Me.btnLength.Size = New System.Drawing.Size(75, 23)
+        Me.btnLength.TabIndex = 2
+        Me.btnLength.Text = "Length"
+        Me.btnLength.UseVisualStyleBackColor = True
+        '
+        'btnToUpper
+        '
+        Me.btnToUpper.Location = New System.Drawing.Point(21, 157)
+        Me.btnToUpper.Name = "btnToUpper"
+        Me.btnToUpper.Size = New System.Drawing.Size(75, 23)
+        Me.btnToUpper.TabIndex = 3
+        Me.btnToUpper.Text = "To Upper"
+        Me.btnToUpper.UseVisualStyleBackColor = True
+        '
+        'btnToLower
+        '
+        Me.btnToLower.Location = New System.Drawing.Point(22, 199)
+        Me.btnToLower.Name = "btnToLower"
+        Me.btnToLower.Size = New System.Drawing.Size(74, 23)
+        Me.btnToLower.TabIndex = 4
+        Me.btnToLower.Text = "To Lower"
+        Me.btnToLower.UseVisualStyleBackColor = True
+        '
+        'btnTrim
+        '
+        Me.btnTrim.Location = New System.Drawing.Point(21, 238)
+        Me.btnTrim.Name = "btnTrim"
+        Me.btnTrim.Size = New System.Drawing.Size(71, 23)
+        Me.btnTrim.TabIndex = 5
+        Me.btnTrim.Text = "Trim "
+        Me.btnTrim.UseVisualStyleBackColor = True
+        '
+        'btnSubstring
+        '
+        Me.btnSubstring.Location = New System.Drawing.Point(172, 76)
+        Me.btnSubstring.Name = "btnSubstring"
+        Me.btnSubstring.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubstring.TabIndex = 6
+        Me.btnSubstring.Text = "Substring"
+        Me.btnSubstring.UseVisualStyleBackColor = True
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Location = New System.Drawing.Point(172, 117)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(75, 23)
+        Me.btnRemove.TabIndex = 7
+        Me.btnRemove.Text = "Remove"
+        Me.btnRemove.UseVisualStyleBackColor = True
+        '
+        'btnReplace
+        '
+        Me.btnReplace.Location = New System.Drawing.Point(172, 157)
+        Me.btnReplace.Name = "btnReplace"
+        Me.btnReplace.Size = New System.Drawing.Size(75, 23)
+        Me.btnReplace.TabIndex = 8
+        Me.btnReplace.Text = "Replace"
+        Me.btnReplace.UseVisualStyleBackColor = True
+        '
+        'btnInsert
+        '
+        Me.btnInsert.Location = New System.Drawing.Point(172, 199)
+        Me.btnInsert.Name = "btnInsert"
+        Me.btnInsert.Size = New System.Drawing.Size(75, 23)
+        Me.btnInsert.TabIndex = 9
+        Me.btnInsert.Text = "Insert"
+        Me.btnInsert.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 295)
+        Me.Controls.Add(Me.btnInsert)
+        Me.Controls.Add(Me.btnReplace)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.btnSubstring)
+        Me.Controls.Add(Me.btnTrim)
+        Me.Controls.Add(Me.btnToLower)
+        Me.Controls.Add(Me.btnToUpper)
+        Me.Controls.Add(Me.btnLength)
+        Me.Controls.Add(Me.btnChars)
+        Me.Controls.Add(Me.txtStrInput)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtStrInput As System.Windows.Forms.TextBox
+    Friend WithEvents btnChars As System.Windows.Forms.Button
+    Friend WithEvents btnLength As System.Windows.Forms.Button
+    Friend WithEvents btnToUpper As System.Windows.Forms.Button
+    Friend WithEvents btnToLower As System.Windows.Forms.Button
+    Friend WithEvents btnTrim As System.Windows.Forms.Button
+    Friend WithEvents btnSubstring As System.Windows.Forms.Button
+    Friend WithEvents btnRemove As System.Windows.Forms.Button
+    Friend WithEvents btnReplace As System.Windows.Forms.Button
+    Friend WithEvents btnInsert As System.Windows.Forms.Button
+
+End Class
---
+++ StringIntro/Form1.vb
@@ -0,0 +1,123 @@
+﻿Public Class Form1
+
+    Private Sub btnChars_Click(sender As Object, e As EventArgs) Handles btnChars.Click
+        'chars shows the character in a certain position in a string 
+        'Chars returns the character at a position in the string specified by the index aka address for that position in that string
+        Dim intIndex As Integer = InputBox("Index:")
+        Dim strInput As String = Me.txtStrInput.Text
+
+        MsgBox("The character at index position " & intIndex & "is: " & strInput.Chars(intIndex))
+    End Sub
+
+    Private Sub btnLength_Click(sender As Object, e As EventArgs) Handles btnLength.Click
+        'length returns a count of characters in a string as an integer
+        Dim strInput As String = Me.txtStrInput.Text
+
+        MsgBox("There are " & strInput.Length & " characters in this string.")
+
+    End Sub
+
+    Private Sub btnToUpper_Click(sender As Object, e As EventArgs) Handles btnToUpper.Click
+        'forces all string letters to be UPPERCASE
+        Dim strInput As String = Me.txtStrInput.Text
+
+        MsgBox(strInput.ToUpper)
+
+    End Sub
+
+    Private Sub btnToLower_Click(sender As Object, e As EventArgs) Handles btnToLower.Click
+        'forces all string letters to be lowercase
+        Dim strInput As String = Me.txtStrInput.Text.ToLower
+        'recommended way to use the tolower/toupper function
+
+        MsgBox(strInput)
+
+    End Sub
+
+    Private Sub btnTrim_Click(sender As Object, e As EventArgs) Handles btnTrim.Click
+        'removes extra leading or trailing spaces (unnecessary spaces at the beginning or end of the text)
+        Dim strInput As String = Me.txtStrInput.Text.Trim
+
+
+        MsgBox(strInput)
+
+    End Sub
+
+    Private Sub btnSubstring_Click(sender As Object, e As EventArgs) Handles btnSubstring.Click
+        'extracts a subset of the actual string and store in a new variable
+        'index is the "address" of a character in a string
+        Dim strInput As String = Me.txtStrInput.Text.Trim
+
+
+        Dim intStartIndex, intSubStrLength As Integer
+        Dim strSubString As String
+
+        intStartIndex = InputBox("Enter the index of the beginning of the substring")
+
+        Do
+            intSubStrLength = InputBox("Enter the number of characters in the substring")
+        Loop Until intStartIndex + intSubStrLength <= strInput.Length
+
+        strSubString = strInput.Substring(intStartIndex, intSubStrLength)
+
+        MsgBox("Substring: " & strSubString)
+
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        'remove part of the original string and store in a new variable
+        Dim strInput As String = Me.txtStrInput.Text.Trim
+
+
+        Dim intStartIndex, intSubStrLength As Integer
+        Dim strStringwithPartRemoved As String
+
+        intStartIndex = InputBox("Enter the index of the beginning of the substring you want removed")
+
+        Do
+            intSubStrLength = InputBox("Enter the number of characters in the substring")
+        Loop Until intStartIndex + intSubStrLength <= strInput.Length
+
+        strStringwithPartRemoved = strInput.Remove(intStartIndex, intSubStrLength)
+
+        MsgBox("Modified original string: " & strStringwithPartRemoved)
+
+    End Sub
+
+    Private Sub btnReplace_Click(sender As Object, e As EventArgs) Handles btnReplace.Click
+        'replace a subset of the actual string with some other string and store in a new variable 
+        Dim strInput As String = Me.txtStrInput.Text.Trim
+
+        Dim strOldString, strNewString As String
+        Dim strReplacedString As String
+
+        strOldString = InputBox("Enter the text to be replaced")
+
+        strNewString = InputBox("Enter the replacement text")
+
+        strReplacedString = strInput.Replace(strOldString, strNewString)
+
+        MsgBox(strReplacedString)
+
+    End Sub
+
+
+    Private Sub btnInsert_Click(sender As Object, e As EventArgs) Handles btnInsert.Click
+        'replace a subset of the actual string with some other string and store in a new variable 
+        Dim strInput As String = Me.txtStrInput.Text.Trim
+
+        Dim intInsertPosition As Integer
+        Dim strStringAddition As String
+        Dim strModifiedString As String
+
+        intInsertPosition = InputBox("Enter the index of the insert start position")
+
+        strStringAddition = InputBox("Enter the text to be inserted")
+
+        strModifiedString = strInput.Insert(intInsertPosition, strStringAddition)
+
+        MsgBox("Your new string is:" & vbCrLf & strModifiedString)
+        'vbtab indents stuff 
+
+    End Sub
+End Class
